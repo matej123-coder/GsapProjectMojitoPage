2187644e9bf292add53c6562faec229885f7eb7e
Matej Minoski <mmatej.minoski@gmail.com>
2026-02-15 16:18:24 +0100

    feat(web)b): adding navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import { useEffect } from "react";
 import gsap from "gsap";
+import { Navbar } from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 export const App = () => {
   return (
-    <div className="flex-center h-[100vh]">
-      <h1 className="text-3xl text-indigo-300">Welcome to Mojito!</h1>
-      <p>Your one-stop solution for all your needs.</p>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
