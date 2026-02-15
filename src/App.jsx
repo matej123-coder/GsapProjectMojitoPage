cf7df9a17ac6451da7ff9b88e8d7302a8c341bf0
Matej Minoski <mmatej.minoski@gmail.com>
2026-02-15 18:13:36 +0100

    feat(web): add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
-import { useEffect } from "react";
 import gsap from "gsap";
 import { Navbar } from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 export const App = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <div className="h-dvh bg-black"></div>
     </main>
   );
 };
